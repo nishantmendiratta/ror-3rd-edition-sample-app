919c1464e521c9d2b2bc6337369cef53c649371e
Nishant Mendiratta <nishuarora.nishant@gmail.com>
2017-01-03 02:39:46 +0530

    updated gravatar_for helper function + added default avatar image

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,12 @@
 module UsersHelper
   # Returns the Gravatar (http://gravatar.com/) for the given user.
   def gravatar_for(user)
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatars/#{gravatar_id}?s=200"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    if user.email?
+      gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+      gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+      image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    else
+      image_tag("/img/avatar_default.jpg", alt: user.name, class: "gravatar")
+    end
   end
 end
